aacb72d1c66cc103abf4bfcc4635cc4c663bfccd
Gennady Stepanov <gennady.stepanoff@gmail.com>
2022-10-16 20:07:47 +0300

    Initial Commit

---
+++ Trantalya/TrantalyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrantalyaApp.swift
+//  Trantalya
+//
+//  Created by Gennady Stepanov on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrantalyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
